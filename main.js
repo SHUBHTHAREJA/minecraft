31de5f2ee847440545822b29b66b1b3424cb8f4a
SHUBHTHAREJA <74976580+SHUBHTHAREJA@users.noreply.github.com>
2021-01-21 20:45:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,3 +34,79 @@ fabric.Image.fromURL(get_image,function(Img){
     canvas.add(block_image_object);
 })
 }
+
+
+window.addEventListener("keydown",my_keydown);
+
+
+function my_keydown(e){
+    keypress=e.keyCode;
+    console.log(keypress);
+    if (e.shiftKey==true && keypress=='80') {
+        console.log("shift and p clicked together");
+        block_image_height= block_image_height+10;
+        block_image_width= block_image_width+10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if (e.shiftKey==true && keypress=='77') {
+        console.log("shift and m clicked together");
+        block_image_height= block_image_height-10;
+        block_image_width= block_image_width-10;
+        document.getElementById("current_width").innerHTML=block_image_width;
+        document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if (keypress=='38' ) {
+        up();
+        console.log("up");
+    }
+    if (keypress=='37' ) {
+        left();
+        console.log("left");
+    }
+    if (keypress=='39' ) {
+        right();
+        console.log("right");
+    }
+    if (keypress=='40' ) {
+        down();
+        console.log("down");
+    }
+    if (keypress=='67' ) {
+        new_image("cloud.jpg");
+        console.log("c");
+    }
+    if (keypress=='68' ) {
+        new_image("dark_green.png");
+        console.log("d");
+    }
+    if (keypress=='71' ) {
+        new_image("ground.png");
+        console.log("g");
+    }
+    if (keypress=='76' ) {
+        new_image("light_green.png");
+        console.log("l");
+    }
+    if (keypress=='82' ) {
+        new_image("roof.jpg");
+        console.log("r");
+    }
+    if (keypress=='84' ) {
+        new_image("trunk.jpg");
+        console.log("t");
+    }
+    if (keypress=='85' ) {
+        new_image("unique.png");
+        console.log("u");
+    }
+    if (keypress=='87' ) {
+        new_image("wall.jpg");
+        console.log("w");
+    }
+    if (keypress=='89' ) {
+        new_image("yellow_wall.png");
+        console.log("y");
+    }
+
+}
