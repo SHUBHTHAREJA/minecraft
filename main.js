f39641b661d0cf045ec83fedd8cb92d0ae026c8c
SHUBHTHAREJA <74976580+SHUBHTHAREJA@users.noreply.github.com>
2021-01-23 11:29:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -51,8 +51,8 @@ function my_keydown(e){
     }
     if (e.shiftKey==true && keypress=='77') {
         console.log("shift and m clicked together");
-        block_image_height= block_image_height-10;
-        block_image_width= block_image_width-10;
+        block_image_height= block_image_height-5;
+        block_image_width= block_image_width-5;
         document.getElementById("current_width").innerHTML=block_image_width;
         document.getElementById("current_height").innerHTML=block_image_height;
     }
@@ -109,4 +109,46 @@ function my_keydown(e){
         console.log("y");
     }
 
+}
+
+function up (){
+    if (player_y>=0) 
+    {
+        player_y=player_y-block_image_height;
+        console.log("x="+player_x+"y="+player_y);
+        console.log("block image height="+block_image_height);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down (){
+    if (player_y<=500) 
+    {
+        player_y=player_y+block_image_height;
+        console.log("x="+player_x+"y="+player_y);
+        console.log("block image height="+block_image_height);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right (){
+    if (player_x<=750) 
+    {
+        player_x=player_x+block_image_width;
+        console.log("x="+player_x+"y="+player_y);
+        console.log("block image width="+block_image_width);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left (){
+    if (player_x>=0) 
+    {
+        player_x=player_x-block_image_width;
+        console.log("x="+player_x+"y="+player_y);
+        console.log("block image width="+block_image_width);
+        canvas.remove(player_object);
+        player_update();
+    }
 }
